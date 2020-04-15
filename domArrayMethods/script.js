5d08ee46ba16bee5838ec0ca9a0b899ef04a9426
Allams <anthony.allams@gmail.com>
2020-04-15 15:16:26 +0100

    Completion of the javascript logic

--- domArrayMethods/script.js
+++ domArrayMethods/script.js
@@ -1,8 +1,9 @@
 const main = document.querySelector("main");
 const addIndividualsBtn = document.querySelector(".add-users");
-const doubleNetworthBtn = document.querySelector(".double-networth");
+const doubleNetworthBtn = document.querySelector(".double-money");
 const showMillionairesBtn = document.querySelector(".show-millionaires");
-const calculateNetworthBtn = document.querySelector(".calculate-networth");
+const sortIndividualsBtn = document.querySelector(".sort-users");
+const calculateNetworthBtn = document.querySelector(".calculate-wealth");
 
 // Array of Individuals and their networth
 let data = [];
@@ -52,9 +53,47 @@ const formatter = new Intl.NumberFormat("en-US", {
   currency: "NGN",
 });
 
+// Function to Double Individuals Networth
+const doubleNetworth = () => {
+  data = data.map((item) => {
+    return { ...item, money: item.money * 2 };
+  });
+  updateDOM();
+};
+
+// Function to Filter Millionaires
+const showMillionaires = () => {
+  data = data.filter((item) => {
+    return item.money >= 1000000;
+  });
+  updateDOM();
+};
+
+// Function to Sort Individuals
+const sortIndividuals = () => {
+  data.sort((a, b) => {
+    return b.money - a.money;
+  });
+  updateDOM();
+};
+
+// Function to calculate the total networth of Individuals
+const calculateNetworth = () => {
+  const net = data.reduce((acc, item) => {
+    return acc + item.money;
+  }, 0);
+  console.log(net);
+  // Add elements and add to DOM
+  const element = document.createElement("div");
+  element.innerHTML = `<h3>Total Networth:<strong>${formatter.format(
+    net
+  )}</strong></h3>`;
+  main.appendChild(element);
+};
+
 // Event Listeners
 addIndividualsBtn.addEventListener("click", addIndividuals);
-// doubleNetworthBtn.addEventListener("click", doubleNetworth);
-// showMillionairesBtn.addEventListener("click", showMillionaires);
-// sortIndividualsBtn.addEventListener("click", sortIndividuals);
-// calculateNetworthBtn.addEventListener("click", calculateNetworth);
+doubleNetworthBtn.addEventListener("click", doubleNetworth);
+showMillionairesBtn.addEventListener("click", showMillionaires);
+sortIndividualsBtn.addEventListener("click", sortIndividuals);
+calculateNetworthBtn.addEventListener("click", calculateNetworth);
